21c06270ee69f826e110a0f8bde5d72f32c30968
Robert Shepherd <robertsh26@outlook.com>
2024-07-06 11:13:54 -0700

    main

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -4,7 +4,7 @@ const checkCarId = async (req, res, next) => {
   try {
     const car = await Car.getById(req.params.id)
     if (!car) {
-      next({ status: 404, message: 'not found!'})
+      next({ status: 404, message: `car with id ${req.params.id} is not found`})
     } else {
       req.car = car
       next()
@@ -15,7 +15,23 @@ const checkCarId = async (req, res, next) => {
 }
 
 const checkCarPayload = (req, res, next) => {
-  
+  if (!req.body.vin) return next({
+    status: 400,
+    message: 'vin is missing',
+  })
+  if (!req.body.make) return next({
+    status: 400,
+    message: 'make is missing',
+  })
+  if (!req.body.model) return next({
+    status: 400,
+    message: 'model is missing',
+  })
+  if (!req.body.mileage) return next({
+    status: 400,
+    message: 'mileage is missing',
+  })
+  next()
 }
 
 const checkVinNumberValid = (req, res, next) => {
